35b64e121f512f7cf243283568416830f0b027e7
illushun <admin@conflictcloud.com>
2024-11-07 18:54:16 +0000

    Updated scheduled command timings

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,9 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('fpl:update-gameweeks')->hourlyAt(10);
-Schedule::command('fpl:update-fixtures')->everyThreeMinutes();
-Schedule::command('fpl:update-teams')->everyThreeMinutes();
-Schedule::command('fpl:update-players')->everyThreeMinutes();
-Schedule::command('fpl:get-dream-team')->hourlyAt(21);
-Schedule::command('fpl:predict-gameweek-fixtures')->hourlyAt(20);
+Schedule::command('fpl:update-gameweeks')->dailyAt('1:00');
+Schedule::command('fpl:update-fixtures')->dailyAt('1:20');
+Schedule::command('fpl:update-teams')->dailyAt('1:40');
+Schedule::command('fpl:update-players')->dailyAt('2:00');
+Schedule::command('fpl:get-dream-team')->dailyAt('2:30');
+Schedule::command('fpl:predict-gameweek-fixtures')->dailyAt('3:00');
