73f79072815cd95c021da5bc058d2aadd3ba2b1d
illushun <admin@conflictcloud.com>
2025-01-25 22:52:34 +0000

    Fix for nav items only showing for mobile

--- resources/views/components/header/navigation.blade.php
+++ resources/views/components/header/navigation.blade.php
@@ -69,13 +69,7 @@
       id="hs-header-classic"
       class="hs-collapse overflow-hidden transition-all duration-300 basis-full grow md:block"
       :class="{ 'hidden': !isOpen }"
-      x-show="isOpen"
-      x-transition:enter="transition transform ease-out duration-300"
-      x-transition:enter-start="opacity-0 -translate-y-4"
-      x-transition:enter-end="opacity-100 translate-y-0"
-      x-transition:leave="transition transform ease-in duration-300"
-      x-transition:leave-start="opacity-100 translate-y-0"
-      x-transition:leave-end="opacity-0 -translate-y-4"
+      x-bind:class="{ 'block': isOpen, 'hidden': !isOpen } md:block"
     >
       <div
         class="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300"
