0768546157dee700bfd0cebf6ed8de9d9653d305
illushun <admin@conflictcloud.com>
2025-02-01 00:48:36 +0000

    Wrong place..

--- routes/console.php
+++ routes/console.php
@@ -10,3 +10,5 @@ Schedule::command('fpl:update-teams')->dailyAt('1:40');
 Schedule::command('fpl:update-players')->dailyAt('2:00');
 Schedule::command('fpl:get-dream-team')->dailyAt('2:30');
 Schedule::command('fpl:predict-gameweek-fixtures')->dailyAt('3:00');
+
+Schedule::command('fpl:get-league-news')->hourly();
